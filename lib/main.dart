205771a2cd06ea29bc04a0f562059729112c06f4
YuriVasconcelosJ <yurivasconcelosavatar@gmail.com>
2024-11-28 15:48:02 -0300

    Criacao da appbar

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,8 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
